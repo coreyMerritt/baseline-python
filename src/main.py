f77667ebaab66e14c1b17e11e4cc503268f3bfcd
Corey Merritt <coreyleemerritt@gmail.com>
2025-10-29 04:25:20 -0400

    Ensure we're only linting the src and tests dirs

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 # Web server hooks
-from src.interfaces.rest.routers import app, routers   # pylint: disable=unused-import
+from src.interfaces.rest.routers import app, routers  # pylint: disable=unused-import
