6597a9e5bbff7f582ce4f200620523dc594b6f2d
Corey Merritt <coreyleemerritt@gmail.com>
2025-10-31 04:28:06 -0400

    Add proper linting test to script; add to ci; add to pre-push hook

--- src/infrastructure/config/config_manager.py
+++ src/infrastructure/config/config_manager.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 import yaml
 from dacite import Config, from_dict
 
---
+++ tests/system/run-linters.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source <(curl -fsS --location "https://raw.githubusercontent.com/coreyMerritt/bash-utils/refs/heads/main/src/main")
+import "bash-test" $@
+
+cdProjectRoot
+deployVenv
+bash "./scripts/install-dependencies.sh" "dev"
+
+btStartTest "isort passes"
+  .venv/bin/python -m isort --check-only ./src/
+
+btStartTest "mypy passes"
+  .venv/bin/python -m mypy ./src/
+
+btStartTest "pylint passes"
+  .venv/bin/python -m pylint --disable=fixme ./src/
+
+btStartTest "ruff passes"
+  .venv/bin/python -m ruff check ./src/
