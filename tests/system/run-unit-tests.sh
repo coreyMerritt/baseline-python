51a6e04f346bfdbcf3add7ba5783fd7548daf744
Corey Merritt <coreyleemerritt@gmail.com>
2025-10-31 05:51:51 -0400

    Add bash-test to linting script

--- tests/system/run-unit-tests.sh
+++ tests/system/run-unit-tests.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 source <(curl -fsS --location "https://raw.githubusercontent.com/coreyMerritt/bash-utils/refs/heads/main/src/main")
+import "bash-test" $@
 
 cdProjectRoot
 deployVenv
 bash "./scripts/install-dependencies.sh" "dev"
-.venv/bin/python -m pytest -v
+
+btStartTest "pytest passes"
+  .venv/bin/python -m pytest -v
