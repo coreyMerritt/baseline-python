286b0e644ee8cc4da3a501ba25795261ed4a5859
Corey Merritt <coreyleemerritt@gmail.com>
2025-11-18 03:04:46 -0500

    Add test to check for unimplemented classes

--- src/domain/exceptions/domain_exception.py
+++ src/domain/exceptions/domain_exception.py
@@ -1,5 +1,6 @@
+from shared.enums.exception_type import ExceptionType
 from shared.exceptions.projectname_exception import ProjectnameException
 
 
 class DomainException(ProjectnameException):
-  ...
+  exception_type = ExceptionType.DOMAIN
---
+++ src/shared/enums/exception_type.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ExceptionType(str, Enum):
+  DOMAIN = "domain"
+  INFRASTRUCTURE = "infrastructure"
+  INTERFACE = "interface"
+  SERVICE = "service"
