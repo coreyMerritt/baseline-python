5c474756f58c832977122e2875691fdca05c0598
Corey Merritt <coreyleemerritt@gmail.com>
2025-11-18 03:24:07 -0500

    Break shared code into _helpers.py; rename test for accuracy

--- tests/system/_helpers.py
+++ tests/system/_helpers.py
@@ -23,15 +23,6 @@ class Module:
 
 
 # Functions
-def main():
-  ensure_in_project_root()
-  source_paths = get_source_paths()
-  classes = get_classes(source_paths)
-  assert_all_classes_are_imported(classes, source_paths)
-  print("All classes are imported. Success.")
-  print("Return 0")
-  return 0
-
 def ensure_in_project_root() -> None:
   project_root = get_project_root()
   os.chdir(project_root)
@@ -108,25 +99,6 @@ def get_filename(path: str) -> str:
 def get_filestem(path: str) -> str:
   return Path(path).stem
 
-def assert_all_classes_are_imported(classes: List[Class], source_paths: List[str]):
-  for class_ in classes:
-    assert is_imported_from_some_path(class_, source_paths), f"Never Imported:\n\t{class_.name}\n\t{class_.path}"
-
-def is_imported_from_some_path(class_: Class, source_paths: List[str]) -> bool:
-  for source_path in source_paths:
-    if is_imported_from_specific_path(class_, source_path):
-      return True
-  return False
-
-def is_imported_from_specific_path(class_: Class, source_path: str) -> bool:
-  with open(source_path, "r", encoding="utf-8") as source_file:
-    lines = source_file.readlines()
-  for line in lines:
-    match = re.match(fr"^from\ .+\ import\ ({class_.name})(?:,|\s|$)", line)
-    if match:
-      return True
-  return False
-
 def bash(cmd_str: str) -> CompletedProcess:
   return run(
     args=cmd_str,
@@ -135,8 +107,3 @@ def bash(cmd_str: str) -> CompletedProcess:
     shell=True,
     text=True
   )
-
-
-# Entrypoint
-if __name__ == "__main__":
-  main()
